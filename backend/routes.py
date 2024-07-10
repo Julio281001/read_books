fccbb598ebe0e5c6ed63bb22fd4dd5dfeee55167
Julio Cesar Estrada Flores <julio@DESKTOP-77J3O3M.localdomain>
2024-07-09 20:39:30 -0600

    Crear rutas necesarias de la api

--- backend/routes.py
+++ backend/routes.py
@@ -24,3 +24,42 @@ def read_books(skip: int = 0, limit: Optional[int] = None, db: Session = Depends
     books = query.all()
     return books
 
+
+@router.get("/books/{book_id}", response_model=Book)
+def read_book(book_id: int, db: Session = Depends(get_db)):
+    db_book = db.query(BookModel).filter(BookModel.id == book_id).first()
+    if db_book is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return db_book
+
+
+@router.post("/books/", response_model=Book)
+def create_book(book: BookCreate, db: Session = Depends(get_db)):
+    db_book = BookModel(title=book.title, author=book.author, rating=book.rating)
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
+
+
+@router.put("/books/{book_id}", response_model=Book)
+def update_book(book_id: int, book: BookCreate, db: Session = Depends(get_db)):
+    db_book = db.query(BookModel).filter(BookModel.id == book_id).first()
+    if db_book is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+    db_book.title = book.title
+    db_book.author = book.author
+    db_book.rating = book.rating
+    db.commit()
+    db.refresh(db_book)
+    return db_book
+
+
+@router.delete("/books/{book_id}", response_model=Book)
+def delete_book(book_id: int, db: Session = Depends(get_db)):
+    db_book = db.query(BookModel).filter(BookModel.id == book_id).first()
+    if db_book is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+    db.delete(db_book)
+    db.commit()
+    return db_book
