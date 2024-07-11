a7b8ad76123f229407e8457d3ccbe52b28d8d7d6
Julio Cesar Estrada Flores <julio@DESKTOP-77J3O3M.localdomain>
2024-07-10 19:14:34 -0600

    Configurar la api para que pueda ser llamada desde un puerto distinto

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from db import engine
 import models
 import routes
+from fastapi.middleware.cors import CORSMiddleware
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -12,6 +13,17 @@ app = FastAPI()
 
 app.include_router(routes.router)
 
+
+origins = ["http://localhost:5500"]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
