38ecefb60a325403b9d0bd4d7a6f52d79c8c29b0
Julio Cesar Estrada Flores <julio@DESKTOP-77J3O3M.localdomain>
2024-07-14 20:10:25 -0600

    Agregar funcionalidad para borrar un libro

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
+async function deleteBook(id) {
+    try {
+        const response = await fetch(`http://localhost:8000/books/${id}`, {
+            method: "DELETE",
+        });
+        if (!response.ok) {
+            throw new Error(`${response.status} ${response.statusText}`);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+    window.location.reload();
+};
+
 document.addEventListener("DOMContentLoaded", async function () {
     const books_list = document.querySelector("#read_books");
     try {
@@ -8,6 +22,15 @@ document.addEventListener("DOMContentLoaded", async function () {
         const books = await response.json();
         books.forEach((book) => {
             const li = document.createElement("li");
+
+            const deleteLink = document.createElement("a");
+            deleteLink.href = "#";
+            deleteLink.textContent = "Delete";
+            deleteLink.addEventListener("click", deleteBook.bind(null, book.id));
+
+            li.appendChild(deleteLink);
+
+            li.appendChild(document.createTextNode(" - "));
 
             const output = book.title + " - " + book.author + " - " + book.rating + "/10";
             const p = document.createElement("p");
